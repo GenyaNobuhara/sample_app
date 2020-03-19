095d6f08e9f38a4ab3f250aaea5655e1fbf0a337
Genya Nobuhara <ball_19_0407@yahoo.co.jp>
2020-03-19 11:57:58 +0900

    make a basic user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200318081816) do
+ActiveRecord::Schema.define(version: 20200319021453) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
